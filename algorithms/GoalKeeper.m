ec165e9e80f88033b2ef1df9c5e1a20ef226ab9c
petr-konovalov <petrkon98@mail.ru>
2018-11-19 23:47:09 +0300

    deleted comments

--- algorithms/GoalKeeper.m
+++ algorithms/GoalKeeper.m
@@ -1,10 +1,8 @@
 %goalkeeper who defends gates using circle trajectory(tactic: long distance goalkeeper)
-%доехать в точку А, развернуться к мячу
-%agent - робот, В - мяч, G - центр ворот
 %RP.Blue(1).rul = GoalKeeperNew(agent, B, G, V);
 
 function rul = GoalKeeper(agent, B, G, V)
-    v = 400; % окрестность ворот, их ширина, радиус окружности
+    v = 400;
     eps = 1e-3;
     radius = 5;
     
@@ -12,25 +10,17 @@ function rul = GoalKeeper(agent, B, G, V)
     if (isempty(tmp))
         tmp = [B.x B.y];
     end 
-    %если мяч подвинулся
+
     if (sqrt((tmp(1) - B.x) ^ 2 + (tmp(2) - B.y) ^ 2) >= radius) && (V(1) * (B.x - G(1)) + V(2) * (B.y - G(2)) >= 0)
-        %находим координаты точки, лежащей на прямой и ближайшей к центру окружности
-        %центр координат смещен к центру ворот
         a = -tmp(2) + B.y;
         b = tmp(1) - B.x;
         c = (B.x - G(1))*(tmp(2) - G(2)) - (tmp(1) - G(1))*(B.y - G(2));
         d = a^2+b^2;
         point = [-a*c/d+G(1), -b*c/d+G(2)];
-        %координаты точки уже в обычных координатах
 
-        %если она внутри круга, т.е. предпологаемая траектория пересекла
-        %окружнось, и надо двигаться
         r_to_point2 = c ^ 2 / d;
         if r_to_point2 + eps < v ^ 2
-            %длина вектора, который надо прибавить к point, чтобы получить
-            %точку пересечения
             length = sqrt(v ^ 2 - r_to_point2);
-            % угол наклона вектора
             %{
             angle = atan(tmp(1) - B.x, tmp(2) - B.y);
             X = point(1) + length * cos(angle);
@@ -45,7 +35,6 @@ function rul = GoalKeeper(agent, B, G, V)
                 fpnt1 = fpnt2;
             end
 
-            %вставить новый move to point!
             rul = MoveToPointForGoalKeeper(agent, fpnt1, B);
         else
             finalPoint = [B.x-G(1), B.y-G(2)];
